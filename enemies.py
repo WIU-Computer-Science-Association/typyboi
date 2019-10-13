1131131c33f96f05e620ab17c4c29596e3b6c033
Ethan Massie <ethan.massie1@gmail.com>
2019-10-13 11:49:53 -0500

    added method comments

--- enemies.py
+++ enemies.py
@@ -1,5 +1,13 @@
 class Enemy:
     def __init__(self, name, hp, damage, gold = 0, items = []):
+        """
+        Keyword arguments:
+        name        -- string name of the enemy
+        hp          -- int health value
+        damage      -- int damage value
+        gold        -- int gold dropped
+        items       -- list of items dropped
+        """
         self.name = name
         self.hp = hp
         self.damage = damage
@@ -7,7 +15,9 @@ class Enemy:
         self.items = items
  
     def is_alive(self):
+        """Return true if the enemies hp is greater than 0"""
         return self.hp > 0
 
     def drop_items(self):
+        """Return enemies gold and items as a tupple"""
         return self.gold, self.items
--- tiles.py
+++ tiles.py
@@ -4,11 +4,23 @@ from typyboi.actions import MoveNorth, MoveEast, MoveSouth, MoveWest, ViewInvent
  
 class MapTile:
     def __init__(self, x, y, flavor_text = ''):
+        """
+        Instantiate a MapTile
+        Keyword arguments:
+        x               -- int x coordinate of tile
+        y               -- int y coordinate of tile
+        flavor_text     -- string text to display for tile
+        """
         self.x = x
         self.y = y
         self.flavor_text = flavor_text
     
     def get_adjacent_moves(self, world):
+        """
+        Return list of tiles adjacent to this one
+        Keyword arguments:
+        world   -- World game world
+        """
         adjacent_moves = []
         x = self.x
         y = self.y
@@ -28,6 +40,11 @@ class MapTile:
         return adjacent_moves
 
     def available_actions(self, world):
+        """
+        Return all available actions at this location
+        Keyword arguments:
+        world   -- World game world 
+        """
         moves = self.get_adjacent_moves(world)
         moves.append(UseHealingItem())
         moves.append(ViewInventory())
@@ -35,15 +52,34 @@ class MapTile:
         return moves
  
     def modify_player(self, player):
+        """
+        Perform actions on player
+        Keyword arguments
+        player  -- Player to modify
+        """
         pass
 
 class LootRoom(MapTile):
     def __init__(self, x, y, flavor_text = "", item_list = [], gold = 0):
+        """
+        Instantiate a MapTile
+        Keyword arguments:
+        x               -- int x coordinate of tile
+        y               -- int y coordinate of tile
+        flavor_text     -- string text to display for tile
+        item_list       -- list of items to give to player
+        gold            -- int gold to give to player
+        """
         self.item_list = item_list
         self.gold = gold
         super().__init__(x, y, flavor_text)
  
     def add_loot(self, player):
+        """
+        Give player loot
+        Keyword arguments:
+        player  -- Player to give items to
+        """
         for item in self.item_list:
             player.inventory.add_item(item)
         player.inventory.add_gold(self.gold)
@@ -51,15 +87,32 @@ class LootRoom(MapTile):
         self.gold = 0
  
     def modify_player(self, player):
+        """
+        Perform actions on player
+        Keyword arguments
+        player  -- Player to modify
+        """
         self.add_loot(player)
 
 class EnemyRoom(MapTile):
     def __init__(self, x, y, room_text, enemy = None):
+        """
+        Instantiate a MapTile
+        Keyword arguments:
+        x               -- int x coordinate of tile
+        y               -- int y coordinate of tile
+        room_text       -- string to display when there is no enemy alive
+        """
         self.enemy = enemy
         self.room_text = room_text
         super().__init__(x, y, room_text)
  
     def modify_player(self, the_player):
+        """
+        Perform actions on player
+        Keyword arguments
+        player  -- Player to modify
+        """
         if self.enemy.is_alive():
             the_player.hp -= self.enemy.damage
             self.flavor_text = (
@@ -71,6 +124,11 @@ class EnemyRoom(MapTile):
             self.flavor_text = self.room_text
 
     def available_actions(self, world):
+        """
+        Return all available actions at this location
+        Keyword arguments:
+        world   -- World game world 
+        """
         if self.enemy.is_alive():
             moves = []
             moves.append(Attack(enemy=self.enemy))
