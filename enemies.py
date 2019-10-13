67e912b566361522d9801906a432c321bd041ad7
Ethan Massie <ethan.massie1@gmail.com>
2019-10-13 11:39:37 -0500

    fixed typo

--- enemies.py
+++ enemies.py
@@ -1,13 +1,13 @@
 class Enemy:
-    def __init__(self, name, hp, damage, gold = 0, itens = []):
+    def __init__(self, name, hp, damage, gold = 0, items = []):
         self.name = name
         self.hp = hp
         self.damage = damage
         self.gold = gold
-        self.itens = itens
+        self.items = items
  
     def is_alive(self):
         return self.hp > 0
 
-    def drop_itens(self):
-        return self.gold, self.itens
+    def drop_items(self):
+        return self.gold, self.items
