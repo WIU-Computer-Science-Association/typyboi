f978ed083b9704a7068337fc45197a2f68d60eec
DhananjayU <42027621+DhananjayU@users.noreply.github.com>
2019-10-15 10:14:39 +0530

    Update actions.py

--- actions.py
+++ actions.py
@@ -2,7 +2,7 @@ from typyboi.player import Player
  
 class Action():
     """
-        Super class for all actions
+    Super class for all actions
     """
     def __init__(self, method, name, hotkey, **kwargs):
         """
@@ -25,59 +25,57 @@ class Action():
 
 class MoveNorth(Action):
  """
-     Class to move the player towards north
+ Class to move the player towards north
  """
     def __init__(self):
         super().__init__(method=Player.move_north, name='Move north', hotkey='n')
  
 class MoveSouth(Action):
  """
-     Class to move the player towards north
+ Class to move the player towards north
  """ 
     def __init__(self):
         super().__init__(method=Player.move_south, name='Move south', hotkey='s')
  
 class MoveEast(Action):
  """
-     Class to move the player towards north
+ Class to move the player towards north
  """ 
     def __init__(self):
         super().__init__(method=Player.move_east, name='Move east', hotkey='e')
  
 class MoveWest(Action):
  """
-     Class to move the player towards north
+ Class to move the player towards north
  """ 
     def __init__(self):
         super().__init__(method=Player.move_west, name='Move west', hotkey='w')
  
 class ViewInventory(Action):
-    """
-       Prints the player's inventory
-    """
+ """
+ Prints the player's inventory
+ """
     def __init__(self):
         super().__init__(method=Player.print_inventory, name='View inventory', hotkey='i')
 
 class EquipWeapon(Action):
  """
-    Class to implement the player equipping a weapon
+ Class to implement the player equipping a weapon
  """
     def __init__(self):
         super().__init__(method=Player.equip_weapon, name='Equip Weapon', hotkey='h')
 
 class UseHealingItem(Action):
  """
-    Class to implement the player using a healing item
+ Class to implement the player using a healing item
  """
-
     def __init__(self):
         super().__init__(method=Player.use_heal_item, name='Use healing item', hotkey='r')
 
 class Attack(Action):
  """
-    Class to make the player attack
+ Class to make the player attack
  """
-
     def __init__(self, enemy):
         super().__init__(method=Player.attack, name="Attack", hotkey='a', enemy=enemy)
 
